0d9a150483a3fe294e31d4b6314610be24ad5965
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-21 04:45:55 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,11 +5,7 @@ class EmailParser
   @emails = emails
 end 
 
-def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')} 
-  returnArr = splitEmail.flatten.uniq
-  returnArr
-end
+
 
 end
 
