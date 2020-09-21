3938d136604d4c0cca94c841cc93e5448c280321
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-21 04:51:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,18 +1,4 @@
-class EmailParser 
-  attr_accessor :name, :csv_emails
-  
- def initialize(csv_emails)
-   @name = name
-   @csv_emails = csv_emails
-  end 
 
- def parse 
-    csv_emails.split.collect do |address|
-      address.split(',') 
-    end
-    .flatten.uniq 
-  end 
-end 
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
 parser1 = EmailParser.new(emails1)
